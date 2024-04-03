fd368697ede576eee24dacd31e5fb39b346704f5
Vitaly Turovsky <vital2580@icloud.com>
2024-04-03 20:21:33 +0300

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,14 @@
 import { defineVitConfig } from '@zardoy/vit'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-export default defineVitConfig({})
+export default defineConfig({
+    base: './',
+    plugins: [
+        react({
+            babel: {
+                configFile: false,
+            },
+        }),
+    ],
+})
